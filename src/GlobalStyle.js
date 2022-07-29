5e194e089dd6a596c76d13150c7c05e1ac0d139f
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-29 10:26:52 +0200

    Add GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-size: 62.5%;
+}
+
+
+`;
+
+export default GlobalStyle;
