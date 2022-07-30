8742a0cdde628d130c066fb99057a91aab6cac25
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-07-30 17:15:55 +0200

    Add fuzzy search

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -4,25 +4,21 @@ import Input from "./Input";
 import { useState } from "react";
 import useStore from "./useStore";
 import SearchSuggest from "./SearchSuggest";
-import { search } from "fast-fuzzy";
+// import { fuzzy, search } from "fast-fuzzy";
+
+const { search } = require("fast-fuzzy");
 
 export default function SearchBar({ labelFor, labelText, placeholder, type }) {
   const [inputValue, setInputValue] = useState("");
   const createShoppingItems = useStore((state) => state.createShoppingItems);
   const fetchedProductItems = useStore((state) => state.fetchedProductItems);
 
+  let fuzzyResults = search(inputValue, fetchedProductItems.data, {
+    keySelector: (obj) => obj.name.de,
+  });
+
   //////////// Fuzzy Search
 
-  const { search } = require("fast-fuzzy");
-
-  function fuzzySearch(inputValue) {
-    let fuzzyResult = search(inputValue, fetchedProductItems.data, {
-      keySelector: (obj) => obj.name.de,
-    });
-    return fuzzyResult;
-  }
-
-  fuzzySearch(inputValue);
   return (
     <>
       <Form
@@ -41,11 +37,10 @@ export default function SearchBar({ labelFor, labelText, placeholder, type }) {
           value={inputValue}
           onChange={(event) => {
             setInputValue(event.target.value);
-            fuzzySearch(inputValue);
           }}
         />
       </Form>
-      <SearchSuggest inputValue={inputValue} />
+      <SearchSuggest inputValue={inputValue} fuzzyResults={fuzzyResults} />
     </>
   );
 }
--- src/Components/SearchSuggest.jsx
+++ src/Components/SearchSuggest.jsx
@@ -4,7 +4,7 @@ import UnorderedList from "./UnorderedList";
 import ListItem from "./ListItem";
 import ListTag from "./ListTag";
 
-export default function SearchSuggest({ inputValue }) {
+export default function SearchSuggest({ inputValue, fuzzyResults }) {
   const fetchProductList = useStore((state) => state.fetchProductList);
   const fetchedProductItems = useStore((state) => state.fetchedProductItems);
   const createShoppingItems = useStore((state) => state.createShoppingItems);
@@ -13,28 +13,28 @@ export default function SearchSuggest({ inputValue }) {
     fetchProductList("https://fetch-me.vercel.app/api/shopping/items");
   }, [fetchProductList]);
 
-  console.log(inputValue);
   return (
     <UnorderedList>
-      {fetchedProductItems.data
+      {/* {fetchedProductItems.data
         .filter((item) => {
           return inputValue.toLowerCase() === ""
             ? item
             : item.name.de.toLowerCase().includes(inputValue);
-        })
-        .map((item) => {
-          return (
-            <ListItem key={item._id}>
-              <ListTag
-                onClick={() => {
-                  createShoppingItems(item.name.de);
-                }}
-              >
-                {item.name.de}
-              </ListTag>
-            </ListItem>
-          );
-        })}
+        }) */}
+
+      {fuzzyResults.map((item) => {
+        return (
+          <ListItem key={item._id}>
+            <ListTag
+              onClick={() => {
+                createShoppingItems(item.name.de);
+              }}
+            >
+              {item.name.de}
+            </ListTag>
+          </ListItem>
+        );
+      })}
     </UnorderedList>
   );
 }
