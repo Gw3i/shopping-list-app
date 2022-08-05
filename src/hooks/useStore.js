369fed5fb1b81de00ad648336a84090ce8148ddc
Vladyslav Nyzhashchyy <contact.johngoldco@gmail.com>
2022-08-05 09:31:53 +0200

    Add persist

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -3,57 +3,57 @@ import create from "zustand";
 import { persist } from "zustand/middleware";
 
 const useStore = create(
-  //persist(
-  (set) => {
-    return {
-      fetchedProductItems: { data: [] },
-      fetchProductList: async (url) => {
-        try {
-          const response = await fetch(url);
-          const data = await response.json();
-          set({ fetchedProductItems: data });
-        } catch (error) {
-          console.error(`Ups, fetchning went wrong: ${error}`);
-        }
-      },
-      categories: { data: [] },
-      addCategories: async (url) => {
-        try {
-          const response = await fetch(url);
-          const data = await response.json();
-          set({ categories: data });
-        } catch (error) {
-          console.error(`Ups, fetchning went wrong: ${error}`);
-        }
-      },
-      shoppingItems: [],
-      createShoppingItems: (name, category) => {
-        set((state) => {
-          return {
-            shoppingItems: [
-              ...state.shoppingItems,
-              { name, _id: nanoid(), category },
-            ],
-          };
-        });
-      },
-      deleteListItems: (id) => {
-        set((state) => {
-          return {
-            shoppingItems: state.shoppingItems.filter(
-              (listItem) => listItem._id !== id
-            ),
-          };
-        });
-      },
-      language: "de",
-      setLanguage: (lang) => {
-        set({ language: lang });
-      },
-    };
-  }
-  //,{ name: "shoppingList" }
-  //)
+  persist(
+    (set) => {
+      return {
+        fetchedProductItems: { data: [] },
+        fetchProductList: async (url) => {
+          try {
+            const response = await fetch(url);
+            const data = await response.json();
+            set({ fetchedProductItems: data });
+          } catch (error) {
+            console.error(`Ups, fetchning went wrong: ${error}`);
+          }
+        },
+        categories: { data: [] },
+        addCategories: async (url) => {
+          try {
+            const response = await fetch(url);
+            const data = await response.json();
+            set({ categories: data });
+          } catch (error) {
+            console.error(`Ups, fetchning went wrong: ${error}`);
+          }
+        },
+        shoppingItems: [],
+        createShoppingItems: (name, category) => {
+          set((state) => {
+            return {
+              shoppingItems: [
+                ...state.shoppingItems,
+                { name, _id: nanoid(), category },
+              ],
+            };
+          });
+        },
+        deleteListItems: (id) => {
+          set((state) => {
+            return {
+              shoppingItems: state.shoppingItems.filter(
+                (listItem) => listItem._id !== id
+              ),
+            };
+          });
+        },
+        language: "de",
+        setLanguage: (lang) => {
+          set({ language: lang });
+        },
+      };
+    },
+    { name: "shoppingList" }
+  )
 );
 
 export default useStore;
